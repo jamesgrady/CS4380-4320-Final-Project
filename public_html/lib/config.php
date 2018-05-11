3c069cd3e5b6acdfe024e4d64beada23deed7f98
jamesgrady <zgrady@gmail.com>
2018-05-11 18:46:07 -0500

    Add files via upload

---
+++ public_html/lib/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// DB account credentials //
+DEFINE("DATABASE", "s18dbmsgroups");
+DEFINE("HOSTNAME", "dbase.dsa.missouri.edu");
+DEFINE("PASSWORD", "hunter");
+DEFINE("USERNAME", "s18group03");
+
+// Used to generate one-time hashes for account registration
+//DEFINE("REGCODE_SECRET", "DoZbex+AhQUCLe7TeB7fWJZsr74x2rfW");
+?>
---
+++ public_html/lib/database.php
@@ -0,0 +1,4 @@
+<?php
+require_once("lib/config.php");
+$db = pg_connect("host=dbase.dsa.missouri.edu dbname=s18dbmsgroups user=s18group03 password=hunter");
+?>
